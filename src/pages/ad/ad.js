2f587af02fcd562a767c19caad55901b66a92371
agang <agang@qq.com>
2021-11-29 10:59:53 +0800

    feat：广告页倒计时跳转功能完成

--- src/pages/ad/ad.js
+++ src/pages/ad/ad.js
@@ -1,4 +1,19 @@
 require('./ad.less')
 
 
-console.log('415145412')
+const countDown = document.querySelector('#countDown')
+const jump = document.querySelector('#jump')
+
+
+let num = 4
+let timer = setInterval(function () {
+    num--
+    countDown.textContent = num + 's'
+    if (num <= 0) {
+        location.href = 'login.html'
+    }
+}, 1000)
+
+jump.addEventListener('click', function () {
+    location.href = 'login.html'
+})
