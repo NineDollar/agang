dbfa85b8a2770e2ecb63ebc79c5bb78760e434a5
agang <agang@qq.com>
2021-11-29 18:19:26 +0800

    feat:首页建框布局

---
+++ src/pages/home/home.js
@@ -0,0 +1,8 @@
+// 引用公共css
+require('../../assets/css/normalize.css')
+require('../../assets/css/reset.css')
+require('../../assets/css/property.css')
+require('../../assets/css/basic.css')
+
+//引入直接的css
+require('./home.less')
