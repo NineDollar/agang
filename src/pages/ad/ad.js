9851b5275d8b0a8d20060cf36984c8701781a78b
agang <agang@qq.com>
2021-11-29 16:02:20 +0800

    feat:实现注册页验证码功能

--- src/pages/ad/ad.js
+++ src/pages/ad/ad.js
@@ -1,21 +1,32 @@
+// 引用公共css
+require('../../assets/css/normalize.css')
+require('../../assets/css/reset.css')
+require('../../assets/css/property.css')
+require('../../assets/css/basic.css')
+
+//引入直接的css
 require('./ad.less')
 
 
-const countDown = document.querySelector('#countDown')
-const jumper = document.querySelector('#jumper')
+
+/* 监听dom 加载完毕 */
+document.addEventListener('DOMContentLoaded', function () {
+    const countDown = document.querySelector('#countDown')
+    const jumper = document.querySelector('#jumper')
 
 
-let num = 4
-let timer = setInterval(function () {
-    num--
-    if (num < 0) {
+    let num = 4
+    let timer = setInterval(function () {
+        num--
+        if (num < 0) {
+            location.href = 'login.html'
+            return
+        }
+        countDown.textContent = num + 's'
+
+    }, 1000)
+
+    jumper.addEventListener('click', function () {
         location.href = 'login.html'
-        return
-    }
-    countDown.textContent = num + 's'
-
-}, 1000)
-
-jumper.addEventListener('click', function () {
-    location.href = 'login.html'
+    })
 })
--- src/pages/reg/reg.js
+++ src/pages/reg/reg.js
@@ -1,1 +1,19 @@
 require('./reg.less')
+
+
+// 引入验证码插件
+const CaptchaMini = require("captcha-mini") // 必须要下载 captcha-mini 才能引入
+
+/* 监听dom加载完毕 */
+document.addEventListener('DOMContentLoaded', function () {
+
+    let code
+
+    // 实例化 引入的插件
+    let captcha = new CaptchaMini();
+    captcha.draw(document.querySelector('#captcha1'), function (r) {
+        // r就是画出来的结果
+        code = r.toUpperCase();
+    });
+    console.log(code);
+})
