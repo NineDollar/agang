01d46aaa27fc82ce4095cdcf196647ac80507e37
agang <agang@qq.com>
2021-11-30 17:09:27 +0800

    feat:首页轮播图完成

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,5 +4,31 @@ require('../../assets/css/reset.css')
 require('../../assets/css/property.css')
 require('../../assets/css/basic.css')
 
+
 //引入直接的css
+require('../../assets/fonts/iconfont.css')
 require('./home.less')
+
+// 引入swiper
+require('../../lib/swiper/swiper-bundle.min.css')
+const Swiper = require('../../lib/swiper/swiper-bundle.min.js')
+
+const axios = require('axios')
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    /* 初始化swiper */
+    const swiper = new Swiper(document.querySelector('.mySwiper'), {
+        loop: true, // 循环
+        autoplay: {  // 自动播放
+            disableOnInteraction: false   // 用户操作了swiper之后 是否禁用自动播放 false 不禁用.
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    })
+
+
+
+
+})
