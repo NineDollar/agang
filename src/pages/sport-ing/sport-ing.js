9518b62bf545f9440920b8d36a7497c8b5ab8679
agang <agang@qq.com>
2021-12-05 20:53:06 +0800

    fead：跑步计时页完成

--- src/pages/sport-ing/sport-ing.js
+++ src/pages/sport-ing/sport-ing.js
@@ -13,24 +13,41 @@ const axios = require('axios')
 
 //引入渲染nav 
 const dom = require('../../utils/dom')
+const date = require('../../utils/date')
 
 document.addEventListener('DOMContentLoaded', function () {
 
 
 
     const stopBtn = document.querySelector('#stopBtn')
-    const stopBtnPanel = document.querySelector('#stopBtnPanel')
     const startBtn = document.querySelector('#startBtn')
-
+    const stopBtnPanel = document.querySelector('#stopBtnPanel')
+    const overBtnPanel = document.querySelector('#overBtnPanel')
+    const timeConsuming = document.querySelector('#timeConsuming')
 
     stopBtn.addEventListener('click', function () {
         stopBtn.style.display = 'none'
         stopBtnPanel.style.display = 'flex'
+        clearInterval(timer)
 
     })
     startBtn.addEventListener('click', function () {
         stopBtn.style.display = 'block'
+        stopBtn.style.animation = 'move 0.3s linear';
         stopBtnPanel.style.display = 'none'
+        timer = setInterval(timeFn, 1000)
     })
+    overBtnPanel.addEventListener('click', function () {
+
+        console.log('结束');
+    })
+
+    let time = 0
+    let timer = setInterval(timeFn, 1000)
+
+    function timeFn() {
+        time += 1
+        timeConsuming.innerHTML = date.secondesToHMS(time)
+    }
 
 })
